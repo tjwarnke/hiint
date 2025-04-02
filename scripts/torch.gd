6df76f7ffa2a994bf1feda7670716877132b9f5a
tjwarnke <t.j.warnke@gmail.com>
2025-04-02 11:22:08 -0500

    fixes

--- scripts/torch.gd
+++ scripts/torch.gd
@@ -4,7 +4,7 @@ signal torch_picked
 
 var player_in_area = null  
 var can_be_picked_up = true
-@onready var text_box = get_node("/root/World/Level/UI/TextBoxMiddleTop")
+@onready var text_box = get_tree().current_scene.find_child("TextBoxMiddleTop", true, false)
 
 
 func _ready(): 
@@ -12,13 +12,14 @@ func _ready():
 
 func _on_body_entered(body):
 	player_in_area = body  
-	if can_be_picked_up:
+	if can_be_picked_up and text_box:
 		text_box.visible = true
 		text_box.text = "Press 'e' to take Torch"
 
 func _on_body_exited(_body):
 	player_in_area = null  
-	text_box.visible = false
+	if text_box:
+		text_box.visible = false
 	
 	
 func _process(_delta):
@@ -26,7 +27,8 @@ func _process(_delta):
 		if player_in_area.held_item != null:
 			return
 		if can_be_picked_up:
-			text_box.visible = false
+			if text_box:
+				text_box.visible = false
 			torch_picked.emit()
 			player_in_area.pick_up_item(self)
 			can_be_picked_up = false
