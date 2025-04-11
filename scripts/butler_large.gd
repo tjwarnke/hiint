55f22319674072515660948418aab503fd36ffd2
Agotz0330 <agotziaman02@outlook.com>
2025-04-10 21:46:13 -0500

    basically finished

--- scripts/butler_large.gd
+++ scripts/butler_large.gd
@@ -6,12 +6,9 @@ var dialogue_active = false
 var player = null
 var cut_scene_seen = false
 
-@onready var text_box = get_node("/root/World/UI/TextBoxMiddleTop")
-@onready var text_box2 = get_node("/root/World/UI/TextBoxMiddleTop2")
-@onready var dialogue = get_node("/root/World/UI/DialogOptions")
+@onready var text = get_node("/root/World/UI/TextBoxMiddleTop")
 @onready var sprite = get_node("/root/World/UI/Speaker")
-@onready var Box1 = get_node("/root/World/UI/TextBoxMiddleTopBack")
-@onready var Box2 = get_node("/root/World/UI/TextBoxMiddleTopBack2")
+
 var butler = preload("res://assets/images/ButlerHead.png")
 
 func _ready():
@@ -26,22 +23,17 @@ func _on_body_entered(body):
 			player_nearby = true
 			sprite.texture = butler
 			sprite.visible = true
-			text_box.visible = true
-			Box2.visible = true
-			text_box.text = "Welcome to the Kusnetzov Mansion!"
+			text.visible = true
+			text.text = "Welcome to the Kusnetzov Mansion!"
 			await get_tree().create_timer(2).timeout
-			text_box.text = "Take a seat at the table!"
+			text.text = "Take a seat at the table!"
 			await get_tree().create_timer(2).timeout
-			text_box.text = "The dinner will start shortly!"
+			text.text = "The dinner will start shortly!"
 			await get_tree().create_timer(2).timeout
-			text_box.visible = false
+			text.visible = false
 			sprite.visible = false
-			Box2.visible = false
 			cut_scene_seen = true
 		
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		player_nearby = false
-		
-		
-		
--- scripts/chair_hit_box.gd
+++ scripts/chair_hit_box.gd
@@ -6,52 +6,45 @@ var dialogue_active = false
 var player = null
 var talked = false
 
-var text_box = null
+var text = null
 var dialogue = null
-var dialogue_box = null
-var text_box_box = null
 var sprite = null
-var text_small = null
 
 var con_head = preload("res://assets/images/conman_head.png")
 var relish_head = preload("res://assets/images/relish_head.png")
 var penguins_head = preload("res://assets/images/penguins_head.png")
+var girl_head = preload("res://assets/images/girl_head.png")
 
 func _ready():
 	area.body_entered.connect(_on_body_entered)
 	area.body_exited.connect(_on_body_exited)
 	
-	text_box = get_node_or_null("../../UI/TextBoxMiddleTop")
+	text = get_node_or_null("../../UI/TextBoxMiddleTop")
 	dialogue = get_node_or_null("../../UI/DialogOptions")
-	dialogue_box = get_node_or_null("../../UI/MeetCharBox")
-	text_box_box = get_node_or_null("../../UI/TextBoxMiddleTopBack2")
 	sprite = get_node_or_null("../../UI/Speaker")
-	text_small = get_node_or_null("../../UI/SmallerBox")
 	
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		player_nearby = true
 		player = body
-		text_box.visible = true
-		text_box.text = "Press 'f' to take a seat"
+		text.visible = true
+		text.text = "Press 'f' to take a seat"
 		
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		player_nearby = false
-		await get_tree().create_timer(1.5).timeout
-		text_box.visible = false
+		await get_tree().create_timer(1).timeout
+		text.visible = false
 		dialogue.visible = false
 		
 func options(response):
 	dialogue.visible = false
-	text_small.visible = true
-	text_box.text = response
+	text.text = response
 	await get_tree().create_timer(2).timeout
 	
 func base_dialog():
 	sprite.visible = false
-	dialogue_box.visible = true
-	dialogue.text = "1. Mysterious Trench Coater \n2. Sargent Relish \n3. Mr. Conman \n4. Leave"
+	dialogue.text = "1. Mysterious Trench Coater \n2. Sargent Relish \n3. Mr. Conman \n4. Girl \n5. Leave"
 	dialogue.visible = true
 	dialogue_active = true
 
@@ -62,57 +55,61 @@ func _input(event):
 		dialogue.visible = false
 
 		if talked:
-			text_box.text = "You already talked to everyone."
-			text_box.visible = true
+			text.text = "You already talked to everyone."
+			text.visible = true
 			player.set_can_move(true)
 			await get_tree().create_timer(1.5).timeout
-			text_box.visible = false
+			text.visible = false
 			
 		else:
-			text_box_box.visible = true
-			text_box.text = "You have taken a seat at the table"
-			text_box.visible = true
+			text.text = "You have taken a seat at the table"
+			text.visible = true
 			await get_tree().create_timer(2).timeout
-			text_box.text = "Who would you like to talk to?"
+			text.text = "Who would you like to talk to?"
 			base_dialog()
 			
 	if dialogue_active:
 			if event.is_action_pressed("Option1"):
-				dialogue_box.visible = false
 				sprite.scale = Vector2(.5, .5)
 				sprite.texture = penguins_head
 				sprite.visible = true
-				text_box.visible = true
+				text.visible = true
 				dialogue.visible = false
-				text_box.text = "Hello I am the Mysterious Trench Coater"
+				text.text = "Hello I am the Mysterious Trench Coater"
 				await get_tree().create_timer(1.5).timeout
-				text_box.text = "Who would you like to talk to?"
+				text.text = "Who would you like to talk to?"
 				base_dialog()
 				
 			if event.is_action_pressed("Option2"):
-				dialogue_box.visible = false
 				sprite.scale = Vector2(.5, .5)
 				sprite.texture = relish_head
 				sprite.visible = true
 				options("Hello I am Sargent Relish")
 				await get_tree().create_timer(1.5).timeout
-				text_box.text = "Who would you like to talk to?"
+				text.text = "Who would you like to talk to?"
 				base_dialog()
 				
 			if event.is_action_pressed("Option3"):
-				dialogue_box.visible = false
 				sprite.scale = Vector2(.5, .5)
 				sprite.texture = con_head
 				sprite.visible = true
 				options("Hello I am the Mr. Conman")
 				await get_tree().create_timer(1.5).timeout
 				sprite.visible = false
-				text_box.text = "Who would you like to talk to?"
+				text.text = "Who would you like to talk to?"
 				base_dialog()
 				
 			if event.is_action_pressed("Option4"):
-				dialogue_box.visible = false
+				sprite.scale = Vector2(.5, .5)
+				sprite.texture = girl_head
+				sprite.visible = true
+				options("Hello I am Girl")
+				await get_tree().create_timer(1.5).timeout
+				sprite.visible = false
+				text.text = "Who would you like to talk to?"
+				base_dialog()
+				
+			if event.is_action_pressed("Option5"): 
 				player.set_can_move(true)
-				text_box.visible = false
+				text.visible = false
 				dialogue.visible = false
-				text_box_box.visible = false
