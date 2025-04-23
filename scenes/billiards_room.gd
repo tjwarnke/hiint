93112128bdf9a6e7108ac370c993c65d8a6bc5e3
DaBigRed <bigred.lundgren@gmail.com>
2025-04-23 16:31:22 -0500

    billiards room logic kind of working

--- scenes/billiards_room.gd
+++ scenes/billiards_room.gd
@@ -8,13 +8,13 @@ var lever2
 var writing
 var poolTable
 var safe
+var safePlatform 
 
 var in_lever1 = false
 var in_lever2 = false
 var in_safe = false
-
 func _ready():
-	safe = get_node("Safe")
+	safe = get_node("Safe/Area2D")
 	chandelierLight = get_node("MidgroundTilemap/Chandalier2/PointLight2D")
 	painting = get_node("MidgroundTilemap/Painting")
 	trapdoor = get_node("DroppingPlatform")
@@ -22,9 +22,17 @@ func _ready():
 	lever2 = get_node("Lever2")
 	writing = get_node("BloodWriting")
 	poolTable = get_node("PoolTable")
+	safePlatform = get_node("SafePlatform")
+	lever1.body_entered.connect( _on_lever_1_body_entered)
+	lever2.body_entered.connect( _on_lever_2_body_entered)
+	safe.body_entered.connect( _on_safe_body_entered)
+	lever1.body_exited.connect( _on_lever_1_body_exited)
+	lever2.body_exited.connect( _on_lever_2_body_exited)
+	safe.body_exited.connect( _on_safe_body_exited)
 	safe.hide()
 	writing.hide()
 	chandelierLight.hide()
+	safePlatform.hide()
 	
 func _input(event: InputEvent) -> void:
 	if in_lever1 and event.is_action_pressed("pick_up"):
@@ -37,11 +45,31 @@ func _input(event: InputEvent) -> void:
 func lever1_action():
 	painting.hide()
 	safe.show()
+	safePlatform.show()
 
 func lever2_action():
 	chandelierLight.show()
 	writing.show()
 	
 func safe_action():
+	#player has to select the numbers 10-31-84
 	pass
+
+func _on_lever_1_body_entered(body):
+	in_lever1 = true
+	
+func _on_lever_2_body_entered(body):
+	in_lever2 = true
+	
+func _on_safe_body_entered(body):
+	in_safe = true
+	
+func _on_lever_1_body_exited(body):
+	in_lever1 = false
+	
+func _on_lever_2_body_exited(body):
+	in_lever2 = false
+	
+func _on_safe_body_exited(body):
+	in_safe = false
 	
